2064b51381d3108be75dea085e1cd195971591a4
Jess <79601842+jess192@users.noreply.github.com>
2021-03-08 15:40:20 -0500

    refactor(client): update to accept logging as an optional argument

--- wh00t_core/library/client_network.py
+++ wh00t_core/library/client_network.py
@@ -12,9 +12,13 @@ class ClientNetwork:
     BUFFER_SIZE: int = 1024
     message_history: List[dict] = []
 
-    def __init__(self, host: str, port: int, app_id: str, app_profile: str, logging_object: Optional) -> NoReturn:
-        self.logger = logging_object.getLogger(type(self).__name__)
-        self.logger.setLevel(logging_object.INFO)
+    def __init__(self, host: str, port: int, app_id: str, app_profile: str, logging_object: Optional = None) -> NoReturn:
+
+        if logging_object:
+            self.logger = logging_object.getLogger(type(self).__name__)
+            self.logger.setLevel(logging_object.INFO)
+        else:
+            self.logger = None
 
         self.number_of_messages: int = 0
         self.client_socket: Any = None
@@ -23,19 +27,30 @@ class ClientNetwork:
         self.app_id: str = app_id
         self.app_profile: str = app_profile
 
+    def log(self, log_type: str, message: str) -> NoReturn:
+        if self.logger:
+            if log_type == 'INFO':
+                self.logger.info(message)
+            elif log_type == 'ERROR':
+                self.logger.error(message)
+            elif log_type == 'WARNING':
+                self.logger.warning(message)
+        else:
+            print(f'{log_type} - {message}')
+
     def sock_it(self) -> NoReturn:
         try:
-            self.logger.info(f'Attempting socket connection to {self.address}')
+            self.log('INFO', f'Attempting socket connection to {self.address}')
             self.client_socket: Any = socket(AF_INET, SOCK_STREAM)
             self.client_socket.connect(self.address)
             package: str = package_data(self.app_id, self.app_profile, f'{self.app_id}_connect', '')
             self.client_socket.send(bytes(package, 'utf8'))
-            self.logger.info(f'Connection to {self.address} has succeeded')
+            self.log('INFO', f'Connection to {self.address} has succeeded')
         except ConnectionRefusedError as connection_refused_error:
-            self.logger.error(f'Received ConnectionRefusedError: {(str(connection_refused_error))}')
+            self.log('ERROR', f'Received ConnectionRefusedError: {(str(connection_refused_error))}')
             os._exit(1)
         except OSError as os_error:  # Possibly client has left the chat.
-            self.logger.error(f'Received an OSError: {(str(os_error))}')
+            self.log('ERROR', f'Received an OSError: {(str(os_error))}')
             os._exit(1)
 
     def send_message(self, message_category: str, message: str) -> NoReturn:
@@ -43,7 +58,7 @@ class ClientNetwork:
             packaged_message: str = package_data(self.app_id, self.app_profile, message_category, message)
             self.client_socket.send(bytes(packaged_message, 'utf8'))
         except IOError as io_error:
-            self.logger.error(f'Received IOError: {(str(io_error))}')
+            self.log('ERROR', f'Received IOError: {(str(io_error))}')
             self.client_socket.close()
             os._exit(1)
 
@@ -55,12 +70,12 @@ class ClientNetwork:
                 self.number_of_messages += 1
                 self.message_history.append(package)
                 self.__trim_message_history()
-                self.logger.info(f'Received Message: {str(package)}')
+                self.log('INFO', f'Received Message: {str(package)}')
             except OSError as os_error:  # Possibly client has left the chat.
-                self.logger.error(f'Received OSError: {(str(os_error))}')
+                self.log('ERROR', f'Received OSError: {(str(os_error))}')
                 break
             except KeyboardInterrupt:
-                self.logger.warning('Received a KeyboardInterrupt... now exiting')
+                self.log('WARNING', 'Received a KeyboardInterrupt... now exiting')
                 self.close_it()
                 os._exit(1)
 
